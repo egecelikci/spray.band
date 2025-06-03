de2a098aca44815effb8602d9498c85836d702a6
Ege Çelikçi <ege@celikci.me>
2025-06-03 05:04:17 +0300

    add serviceworker

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,7 +1,11 @@
 // Module Preload
 import "vite/modulepreload-polyfill";
 
+// Main Stylesheet
 import "../styles/index.css";
 import "../styles/prism-diff.css";
+
+// Common Modules
+import "./common/register-serviceworker";
 
 import "/node_modules/@zachleat/heading-anchors/heading-anchors.js";
